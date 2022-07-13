708162c6e9cef286a8e90f0bdfa3a7413c8d9734
abhyasa <92961553+abhyasa@users.noreply.github.com>
2022-07-13 15:16:10 +0700

    halo comit pembuatan buat nota

---
+++ app/src/main/java/com/example/notadinas/BuatNotaActivity.java
@@ -0,0 +1,14 @@
+package com.example.notadinas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BuatNotaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buat_nota);
+    }
+}
