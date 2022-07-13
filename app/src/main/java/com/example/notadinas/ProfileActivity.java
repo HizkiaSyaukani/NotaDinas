0b0eece7e982e50f356bc8a9d643043d7854180a
hizkiasyaukani <hizkiapro980@gmail.com>
2022-07-13 11:36:12 +0700

    Buat Profil Pertama

--- app/src/main/java/com/example/notadinas/ProfileActivity.java
+++ app/src/main/java/com/example/notadinas/ProfileActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
