03724655fe0f3078503122f354c3f652de6f5b60
abhyasa <92961553+abhyasa@users.noreply.github.com>
2022-07-18 09:50:06 +0700

    note revisi, preview surat masuk

---
+++ app/src/main/java/com/example/notadinas/PreviewSuratMasukActivity.java
@@ -0,0 +1,14 @@
+package com.example.notadinas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PreviewSuratMasukActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview_surat_masuk);
+    }
+}
