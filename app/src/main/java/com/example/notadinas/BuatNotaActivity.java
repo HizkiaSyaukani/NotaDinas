e1db17e5c9308d8e85afd3c2828d2584fd7edfd0
abhyasa <92961553+abhyasa@users.noreply.github.com>
2022-07-19 16:01:21 +0700

    rakha ngepush

--- app/src/main/java/com/example/notadinas/BuatNotaActivity.java
+++ app/src/main/java/com/example/notadinas/BuatNotaActivity.java
@@ -3,12 +3,40 @@ package com.example.notadinas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class BuatNotaActivity extends AppCompatActivity {
+public class BuatNotaActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buat_nota);
+
+        Spinner Pengirim = findViewById(R.id.dari );
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Departemen_Pengirim, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Pengirim.setAdapter(adapter);
+        Pengirim.setOnItemSelectedListener(this);
+
+        Spinner Tujuan = findViewById(R.id.pilih_tujuan );
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.Departemen_Tujuan, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Tujuan.setAdapter(adapter1);
+        Tujuan.setOnItemSelectedListener(this);
     }
-}
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    }
