14a05bf3feb4d21264407741d287a53c18fc94f0
hizkiasyaukani <hizkiapro980@gmail.com>
2022-07-19 08:13:11 +0700

    Profil Selesai

--- app/src/main/java/com/example/notadinas/ProfileActivity.java
+++ app/src/main/java/com/example/notadinas/ProfileActivity.java
@@ -6,11 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class ProfileActivity extends AppCompatActivity {
+    BottomNavigationView bnv;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        BottomNavigationView bnv = (BottomNavigationView) findViewById(R.id.bottomnavlogin);
         
     }
 }
