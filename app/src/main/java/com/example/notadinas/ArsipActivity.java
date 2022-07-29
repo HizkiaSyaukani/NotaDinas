549a88cb174d9e5a1f4004617f2d268be9f12e1f
Hizkia Syaukani <hizkiapro980@gmail.com>
2022-07-29 13:29:16 +0700

    Duplikasi Surat Masuk kedalam Arsip dan Surat Keluar

---
+++ app/src/main/java/com/example/notadinas/ArsipActivity.java
@@ -0,0 +1,14 @@
+package com.example.notadinas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ArsipActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arsip);
+    }
+}
